a5ad462e162beb97eb3c97323a74568e3d428b79
Chris Mercado <cegmercado@gmail.com>
2020-06-08 19:34:18 +0800

    few lines of code to add UN

--- como-ph-exercise.R
+++ como-ph-exercise.R
@@ -34,3 +34,58 @@ population_psa_2015[r, c]
 population_psa_2015[100, ]
 
 ##
+
+
+##
+
+## June 8, 2020 session
+
+## Pulling data ----------------------------
+
+## DataDropPH ------------------------------
+
+ph_get_cases()
+ph_get_cases(date = "2020-06-07")
+
+
+## PSA2015 ---------------------------------
+
+##https://psa.gov.ph/sites/default/files/attachments/hsd/pressrelease/
+##Updated%20Population%20Projections%20based%20on%202015%20POPCEN_0.xlsx
+
+link <- "https://psa.gov.ph/sites/default/files/attachments/hsd/pressrelease/"
+fname <- "Updated%20Population%20Projections%20based%20on%202015%20POPCEN_0.xlsx"
+
+data_psa_2015 <- ph_get_psa2015_pop(file = paste(link, fname, sep = ""))
+
+data_psa_2015
+
+## tibble to data frame 
+data.frame(data_psa_2015)
+as.data.frame(data_psa_2015)
+
+## Use of index - get row 100, all columns -
+
+data_psa_2015[100, ]
+names(data_psa_2015)
+
+## all rows of data year 2020
+data_psa_2015[data_psa_2015$year == 2020, ]
+## all rows of data age 0-4
+data_psa_2015[data_psa_2015$age_category == "0-4", ]
+## all rows 2020 and 5-9
+
+data_psa_2015[data_psa_2015$year == 2020 & data_psa_2015$age_category == "5-9", 4]
+data_psa_2015[data_psa_2015$year == 2020 & data_psa_2015$age_category == "5-9", "total"]
+
+## with function 
+
+data_psa_2015[with(data_psa_2015, year == 2020 & age_category == "5-9"), 4]
+
+
+## UN 2019 ---------------------------------
+
+
+
+
+
