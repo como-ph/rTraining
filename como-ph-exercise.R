dc54bf4cdc35101362980290754ac9fce68b8af4
Ernest Guevarra <5742010+ernestguevarra@users.noreply.github.com>
2020-06-09 11:50:12 +0100

    update como-ph-exercise.R

--- como-ph-exercise.R
+++ como-ph-exercise.R
@@ -18,7 +18,7 @@ library(comoparams)
 
 ## Get population data ----------
 
-## Some notes on the topic
+## Some notes on the topic - This is the approach to create objects in R
 link <- "https://psa.gov.ph/sites/default/files/attachments/hsd/pressrelease/"
 fname <- "Updated%20Population%20Projections%20based%20on%202015%20POPCEN_0.xlsx"
 
@@ -27,10 +27,55 @@ paste(link, fname, sep = "")
 ## Use function
 population_psa_2015 <- ph_get_psa2015_pop(file = paste(link, fname, sep = ""))
 
+## converting to data.frame
+data.frame(population_psa_2015)
+as.data.frame(population_psa_2015)
+
 ## Template syntax
-population_psa_2015[r, c]
+#population_psa_2015[r, c]
 
 ## Use of index - get row 100, all columns
 population_psa_2015[100, ]
 
-##
+## Get all rows of data for year 2020
+population_psa_2015[population_psa_2015$year == 2020, ]
+
+## Get all rows of data for age group 0-4
+population_psa_2015[population_psa_2015$age_category == "0-4", ]
+
+## Get all rows of data for year 2020 and age group = 5-9 total population
+population_psa_2015[population_psa_2015$year == 2020 & population_psa_2015$age_category == "5-9", 4]
+population_psa_2015[population_psa_2015$year == 2020 & population_psa_2015$age_category == "5-9", "total"]
+
+population_psa_2015[population_psa_2015$year == 2020 & population_psa_2015$age_category == "5-9", ]
+
+## with function
+population_psa_2015[with(population_psa_2015, year == 2020 & age_category == "5-9"), 4]
+population_psa_2015[with(population_psa_2015, year == 2020 & age_category == "5-9"), "total"]
+
+population_psa_2015[with(population_psa_2015, year == 2020 & age_category == "5-9"), ]
+
+with(population_psa_2015, total[year == 2020 & age_category == "5-9"])
+
+
+## Using dplyr to arrive at the same output above
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(magrittr)) install.packages("magrittr")
+
+
+## Get population for 2020
+population_psa_2015 %>% 
+  filter(year == 2020)        ## filter rows of data based on a condition
+  
+## Get population for age group 0-4
+population_psa_2015 %>%
+  filter(age_category == "0-4")
+
+## Get population for year 2020 and age group 5-9
+population_psa_2015 %>%
+  filter(year == 2020 & age_category == "5-9") %>%
+  select(total)              ## select columns of data
+
+
+
+
